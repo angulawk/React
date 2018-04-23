7c1160f6b6a9e8a8ddc96e75153b1d225eca7cf1
Agnieszka Wójcik <agnieszka.wojcik@ac-project.net>
2018-04-23 21:59:23 +0200

    Add inline styles to button

--- src/App.js
+++ src/App.js
@@ -29,18 +29,44 @@ class App extends Component {
     ]});
   }
 
+  handleChangeName = (event) => {
+    this.setState({persons: [
+      {
+        name: "Aga",
+        age: 24
+      },
+      {
+        name: event.target.value,
+        age: 25
+      }
+    ]});
+  }
+
   render() {
+    const switchName = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid black",
+      padding: "8px"
+    };
+  
     return (
       <div className="App">
-        <button onClick={() => this.switchNameHandler("Bobo")}>Switch name</button>
+        <button 
+          style={switchName}
+          onClick={() => this.switchNameHandler("Bobo")}>
+          Switch name
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
           click={this.switchNameHandler.bind(this, "Coco")}/>
         <Person
+          style={personalDetails}
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "Kico")}>Hobby: reading books</Person>
+          click={this.switchNameHandler.bind(this, "Kico")}
+          onChange={this.handleChangeName}>Hobby: reading books</Person>
       </div>
     );
   }
